c53c321fef4043c4fd076aa66945d7073dd59d12
DaCoderMane <Carterwombolt123@gmail.com>
2024-08-12 01:03:55 -0400

    Update AbyssInjectionMessage.lua

--- AbyssInjectionMessage.lua
+++ AbyssInjectionMessage.lua
@@ -1,7 +1,7 @@
 local players = game:GetService("Players")
 game:GetService("StarterGui"):SetCore("SendNotification", {
     Title = "Abyss",
-    Text = "Injected Successfully. Enjoy, " .. players.LocalPlayer.DisplayName .. "!",
+    Text = "Successfully Injected. Enjoy, " .. players.LocalPlayer.DisplayName .. "!",
     Icon = "rbxthumb://type=AvatarHeadShot&id=" .. players.LocalPlayer.UserId .. "&w=180&h=180",
     Duration = 5
 })
